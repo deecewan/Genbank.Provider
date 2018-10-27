312e8a475328f00a8c28b7662426944b73c05f70
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-10-28 00:58:07 +1000

    add a singleton helper

--- src/Genbank.DesignTime/Helpers.fs
+++ src/Genbank.DesignTime/Helpers.fs
@@ -9,24 +9,25 @@ let BaseFile: FTP.FileItem =
     variant = FTP.Directory
     location = "ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/" }
 
-type AssemblyLocation = 
+type AssemblyLocation =
   { name: string
     file: string
   }
+
+let (|Singleton|) = function [l] -> l | _ -> failwith "Parameter mismatch"
 
 let getLatestAssembliesFor(genome: FTP.FileItem) =
   let latestItem = genome.childDirectory("latest_assembly_versions")
   // in the latest assembly location, there should only ever be one item
   let items = latestItem |> FTP.loadDirectory
   let length = List.length(items)
-  if length = 0 then 
+  if length = 0 then
     let message =
-      sprintf "Couldn't get latest assembly for %A at %s" genome 
+      sprintf "Couldn't get latest assembly for %A at %s" genome
         latestItem.location
     logger.Error ("%s") message
     message |> failwith
   items |> List.map(fun item -> { name = item.name; file = item.childFile(item.name + "_genomic.gbff.gz").location })
-
 
 let loadGenomesForTaxon(variant: FTP.FileItem) = variant |> FTP.getChildDirectories
 let loadTaxa() = FTP.getChildDirectories(BaseFile)
@@ -41,5 +42,3 @@ let loadGenbankFile (location: string) callback =
   let s =
     Bio.IO.GenBank.GenBankParser().Parse(stream) |> Seq.cast<Bio.ISequence>
   callback s
-
-let (|Singleton|) = function [l] -> l | _ -> failwith "Parameter mismatch"
