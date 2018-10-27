32f4bd8920c35a973a3a4d51337d108f97601008
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-10-28 03:36:40 +1000

    make the runtime types actually load data

--- src/Genbank.RunTime/Library.fs
+++ src/Genbank.RunTime/Library.fs
@@ -1,20 +1,30 @@
-module Genbank.RunTime
+namespace Genbank.RunTime
 
 open Bio.IO.GenBank
 open System.Collections
+open Genbank.Shared
 
 type LociMap (dataURL: string) =
   // let metadata = seq { for item in data -> item.Metadata.Item("GenBank") :?> GenBankMetadata}
-  let metadata = ["test"; dataURL] |> List.toSeq
-  interface seq<string> with member __.GetEnumerator() = metadata.GetEnumerator()
+  let metadata = Helpers.loadGenbankFile(dataURL)(fun items ->
+     items |> Seq.map(fun item -> item.Metadata.Item("GenBank") :?> GenBankMetadata)
+  )
+  interface seq<GenBankMetadata> with member __.GetEnumerator() = metadata.GetEnumerator()
   interface IEnumerable with member __.GetEnumerator() = metadata.GetEnumerator() :> _
 
-type AssemblyType (name: string, url: string) =
-  member __.Name = name;
+type AssemblyType (url: string) =
   member __.Location = url;
 
   member __.LoadLocus(name: string) =
-    name
+    Helpers.loadGenbankFile(url)(fun items ->
+      items
+      |> Seq.map(fun item -> item.Metadata.Item("GenBank") :?> GenBankMetadata)
+      |> Seq.find(fun item -> item.Locus.Name = name)
+    )
 
-  member this.LociMap() =
-    LociMap(this.Location)
+type Taxon (name, location) =
+  member __.Name = name
+  member __.Location = location
+
+type Taxa () =
+  member __.Test = 1
