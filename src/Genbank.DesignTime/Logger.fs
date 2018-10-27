64ae923e0cfca671e45a37488541579e5ccbab28
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-10-28 00:57:51 +1000

    just disable the logger - Forest is causing too many issues right now

--- src/Genbank.DesignTime/Logger.fs
+++ src/Genbank.DesignTime/Logger.fs
@@ -3,11 +3,19 @@ module Genbank.DesignTime.Logger
 open System.IO
 open System
 
-let console = new Forest.ConsoleWriter.ConsoleWriter()
-let logFolder =
-  Path.Combine
-    (Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Logs",
-     "Genbank.Provider")
-let file = new Forest.FileWriter.FileWriter(logFolder, true)
-let logger = Forest.create ("Genbank.Provider") [(*console; file *)]
-let createChild = Forest.createChild
+//let console = new Forest.ConsoleWriter.ConsoleWriter()
+//let logFolder =
+//  Path.Combine
+//    (Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Logs",
+//     "Genbank.Provider")
+//let file = new Forest.FileWriter.FileWriter(logFolder, true)
+// let logger = Forest.create ("Genbank.Provider") [(*console; file *)]
+//let createChild = Forest.createChild
+
+type Logger() =
+  member __.Log(msg) = printfn(msg)
+  member __.Warn(msg) = printfn(msg)
+  member __.Error(msg) = printfn(msg)
+
+let logger = Logger()
+let createChild logger name = Logger()
