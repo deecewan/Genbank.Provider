2eaff15dcb3d7281f594c8ef282e6386acbfe528
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-10-28 00:57:34 +1000

    make the failure message more obvious if the provider fails to load

--- src/Genbank.DesignTime/Library.fs
+++ src/Genbank.DesignTime/Library.fs
@@ -3,14 +3,19 @@ namespace Genbank.DesignTime
 open ProviderImplementation.ProvidedTypes
 open Microsoft.FSharp.Core.CompilerServices
 open System.Reflection
+open System
 
 [<TypeProvider>]
 type GenbankProvider(config: TypeProviderConfig) as this =
   inherit TypeProviderForNamespaces(config)
   let ns = "Genbank.Provider"
   let asm = Assembly.GetExecutingAssembly()
-  
-  do this.AddNamespace(ns, TypeGenerators.createTaxaTypes(asm, ns))
+
+  do
+    try
+      this.AddNamespace(ns, TypeGenerators.createTaxaTypes(asm, ns))
+    with
+    | x -> failwith(sprintf("you dun goofed %A")(x))
 
 [<assembly:TypeProviderAssembly>]
 do ()
